7fa36ea8240ddd6402ae374813fce8c93415c1e0
Wang Qiu <winsonwq@gmail.com>
2014-04-06 22:35:09 +0800

    create game

---
+++ index.js
@@ -0,0 +1,10 @@
+var Game = require('./lib/game');
+var game = new Game(process.stdin, process.stdout);
+game.start();
+
+process.stdin.setRawMode(true);
+process.stdin.on('keypress', function(ch, key) {
+  if (key.ctrl && key.name == 'c') {
+    process.exit(0);
+  }
+});
