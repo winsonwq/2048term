6ba6b01b8cd03361240e9f29248ec0199a5d7f1b
Wang Qiu <winsonwq@gmail.com>
2014-04-06 15:00:26 +0800

    left to right row calculation

---
+++ test/row_calc_spec.js
@@ -0,0 +1,38 @@
+const should = require('chai').should();
+const rowCalc = require('../lib/row_calc');
+
+describe('row calculator', function() {
+
+  describe('left to right calculation', function() {
+
+    it('could merge [0, 2] to [0, 2]', function() {
+      rowCalc.merge([0, 2]).should.eql([0, 2]);
+    });
+
+    it('could merge [2, 2] to [0, 4]', function() {
+      rowCalc.merge([2, 2]).should.eql([0, 4]);
+    });
+
+    it('could merge [0, 0, 2] to [0, 0, 2]', function() {
+      rowCalc.merge([0, 0, 2]).should.eql([0, 0, 2]);
+    });
+
+    it('could merge [0, 2, 2] to [0, 0, 4]', function() {
+      rowCalc.merge([0, 2, 2]).should.eql([0, 0, 4]);
+    });
+
+    it('could merge [2, 2, 2] to [0, 2, 4]', function() {
+      rowCalc.merge([2, 2, 2]).should.eql([0, 2, 4]);
+    });
+
+    it('could merge [2, 2, 2, 2] to [0, 0, 4, 4]', function() {
+      rowCalc.merge([2, 2, 2, 2]).should.eql([0, 0, 4, 4]);
+    });
+
+    it('could merge [2, 4, 2, 4] to [2, 4, 2, 4]', function() {
+      rowCalc.merge([2, 4, 2, 4]).should.eql([2, 4, 2, 4]);
+    });
+
+  });
+
+});
